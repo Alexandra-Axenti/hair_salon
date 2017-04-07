ab9774034daf923a956f439b85e77af4e2811975
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-07 10:42:48 +0800

    add specs for class Client

---
+++ spec/client_spec.rb
@@ -0,0 +1,59 @@
+require('spec_helper')
+
+describe(Client) do
+  describe('.all') do
+    it("starts off with no clients") do
+      expect(Client.all()).to(eq([]))
+    end
+  end
+
+  describe("#initialize") do
+    it("is initialized with a name") do
+      client = Client.new({:name => "Emil Ross", :id => nil})
+      expect(client).to(be_an_instance_of(Client))
+    end
+
+    it("can be initialized with its database ID") do
+      client = Client.new({:name => "Emil Ross", :id => 1})
+      expect(client).to(be_an_instance_of(Client))
+    end
+  end
+
+  describe(".find") do
+    it("returns a client by its ID number") do
+      test_client = Client.new({:name => "Emil Ross", :id => nil})
+      test_client.save()
+      test_client2 = Client.new({:name => "Jason Black", :id => nil})
+      test_client2.save()
+      expect(Client.find(test_client2.id())).to(eq(test_client2))
+    end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same name and id") do
+      client = Client.new({:name => "Emil Ross", :id => nil})
+      client2 = Client.new({:name => "Emil Ross", :id => nil})
+      expect(client).to(eq(client2))
+    end
+  end
+
+  describe("#update") do
+    it("lets you update clients in the database") do
+      client = Client.new({:name => "Emil Ross", :id => nil})
+      client.save()
+      client.update({:name => "Emilio Ross"})
+      expect(client.name()).to(eq("Emilio Ross"))
+    end
+  end
+
+  describe("#delete") do
+    it("lets you delete a client from the database") do
+      client = Client.new({:name => "John Snow", :id => nil})
+      client.save()
+      client2 = Client.new({:name => "John Hair", :id => nil})
+      client2.save()
+      client.delete()
+      expect(Client.all()).to(eq([client2]))
+    end
+  end
+end
