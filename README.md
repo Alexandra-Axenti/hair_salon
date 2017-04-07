5c022e3de3995267ac2bc5c40f9a0b1c1be17b54
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-04-07 10:33:29 +0800

    add specs for class Stylist

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hair_salon
-A web app to manage a hair salon's stylists and clients
+
+A web app to manage a hair salon's stylists and clients.
+
+# User Stories
+
+* As a salon owner, I want to view, add, update and delete stylists and clients.
+* As a salon owner, I want to view, add, update and delete clients.
+* As a salon owner, I want to add clients to a stylist.
+
+# Instructions for DataBase Setup:
+
+* Install postgres
+* Open your terminal and run the following commands:
+
+CREATE DATABASE hair_salon;
+
+CREATE TABLE stylists (id  serial PRIMARY KEY, name VARCHAR);
+
+CREATE TABLE clients (id  serial PRIMARY KEY, name VARCHAR, stylist_id INTEGER);
+
+ALTER TABLE clients ADD FOREIGN KEY (stylist_id) REFERENCES stylists (id);
+
+CREATE DATABASE hair_salon_test WITH TEMPLATE hair_salon;
---
+++ spec/stylist_spec.rb
@@ -0,0 +1,82 @@
+require('spec_helper')
+
+describe(Stylist) do
+  describe('.all') do
+    it("starts off with no stylists") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe("#initialize") do
+    it("is initialized with a name") do
+      stylist = Stylist.new({:name => "Luigi Capelli", :id => nil})
+      expect(stylist).to(be_an_instance_of(Stylist))
+    end
+
+    it("can be initialized with its database ID") do
+      stylist = Stylist.new({:name => "Sandra Rose", :id => 1})
+      expect(stylist).to(be_an_instance_of(Stylist))
+    end
+  end
+
+  describe(".find") do
+    it("returns a stylist by its ID number") do
+      test_stylist = Stylist.new({:name => "John Hair", :id => nil})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:name => "John Snow", :id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+    end
+  end
+
+  describe("#==") do
+    it("is the same stylist if it has the same name and id") do
+      stylist = Stylist.new({:name => "John Snow", :id => nil})
+      stylist2 = Stylist.new({:name => "John Snow", :id => nil})
+      expect(stylist).to(eq(stylist2))
+    end
+  end
+
+  describe("#update") do
+    it("lets you update stylists in the database") do
+      stylist = Stylist.new({:name => "John Snow", :id => nil})
+      stylist.save()
+      stylist.update({:name => "John Hair"})
+      expect(stylist.name()).to(eq("John Hair"))
+    end
+
+    it("lets you add a client to a stylist") do
+      client = Client.new({:name => "Emma Wong", :id => nil})
+      client.save()
+      stylist = Stylist.new({:name => "John Hair", :id => nil})
+      stylist.save()
+      stylist.update({:client_ids => [client.id()]})
+      expect(stylist.clients()).to(eq([client]))
+    end
+  end
+
+  describe("#clients") do
+    it("returns all of the clients scheduled to a particular stylist") do
+      client = Client.new(:name => "Lucy Liu", :id => nil)
+      client.save()
+      client2 = Client.new(:name => "Bing Bing", :id => nil)
+      client2.save()
+      stylist = Stylist.new(:name => "John Hair", :id => nil)
+      stylist.save()
+      stylist.update(:client_ids => [client.id()])
+      stylist.update(:client_ids => [client2.id()])
+      expect(stylist.clients()).to(eq([client, client2]))
+    end
+  end
+
+  describe("#delete") do
+    it("lets you delete a stylist from the database") do
+      stylist = Stylist.new({:name => "John Snow", :id => nil})
+      stylist.save()
+      stylist2 = Stylist.new({:name => "John Hair", :id => nil})
+      stylist2.save()
+      stylist.delete()
+      expect(Stylist.all()).to(eq([stylist2]))
+    end
+  end
+end
