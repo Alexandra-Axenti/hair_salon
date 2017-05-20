7af886a765b879ca14a176881f6a481d760d6d97
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-05-21 00:23:05 +0800

    add first integration spec and add MIT License to README file

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,8 +4,10 @@ require('capybara/rspec')
   set(:show_exceptions, false)
 
   describe('navigating to index page', {:type => :feature}) do
-    it('allows a user to view the index page') do
+    it('allows the admin to view all clients and stylists on the index page') do
       visit('/')
-      expect(page).to have_content('Hello!')
+      expect(page).to have_content(Stylist.all())
+      expect(page).to have_content(Client.all())
+      expect(page).to have_content('Add Stylist','Add Client')
     end
   end
